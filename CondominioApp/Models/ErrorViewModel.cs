3eb8f4769b70f82ba97d9c7d9eb13e0f6f5720de
delga <delga@DARIANCAMACHO>
2023-09-26 22:05:12 -0600

    Add project files.

---
+++ CondominioApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CondominioApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
